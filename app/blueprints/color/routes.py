7c23b6e28be37c14d59dba30b92cbeceab8f4833
Justin Rittenhouse <rittenhouse.justin@gmail.com>
2022-02-16 10:02:22 -0500

    more colors for the joke

--- app/blueprints/color/routes.py
+++ app/blueprints/color/routes.py
@@ -9,7 +9,22 @@ def red():
     # return render_template('red.html', title='Red')
     return "Some things are colors, like red."
 
+@color.route('/yellow')
+def blue():
+    # return render_template('yellow.html', title='Yellow')
+    return "Some things are colors, like yellow."
+
+@color.route('/green')
+def blue():
+    # return render_template('green.html', title='Green')
+    return "Some things are colors, like green."
+
 @color.route('/blue')
 def blue():
     # return render_template('blue.html', title='Blue')
     return "Some things are colors, like blue."
+
+@color.route('/purple')
+def blue():
+    # return render_template('purple.html', title='Purple')
+    return "Some things are colors, like purple."
